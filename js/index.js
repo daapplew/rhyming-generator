e58458d582652a96f82f3bb824ee21f5d95c5c64
Dwayne Applewhite <dwayneaapplewhite@gmail.com>
2022-07-14 16:38:33 -0400

    added comments to code

--- js/index.js
+++ js/index.js
@@ -1,28 +1,23 @@
 const searchInput = document.getElementById('rhyme-input');
 const searchBtn = document.querySelector('button');
 const pText = document.querySelector('p');
+const section = document.getElementById('rhyme-output');
 
-const api = `https://api.datamuse.com/words?rel_rhy=`;
+const api = `https://api.datamuse.com/words?rel_rhy=`; //api url
 
-searchBtn.addEventListener("click", function(){
+searchBtn.addEventListener("click", getWord);
 
-    getWord();
-
+/** 
+ * gets user input value and fetches the api and returns word in paragraph
+ */ 
 async function getWord(){
+    pText.textContent= ""; //clear paragraph
     const response = await fetch(api + searchInput.value);
     const data = response.json();
     for (let i =0; i < 9;i++)
-    data.then((data) => pText.textContent += data[i].word + " ");
-
+        data.then((data) => pText.textContent += data[i].word + " ")
 }
 
-})
-
-// function handler(event){
-//     getWord();
-//     event.type
-// }
-
-
-
-
+/**
+ * function creates a span 
+ */
