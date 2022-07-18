6fadd1888a52b0c645edd14d64b09fbf6c574b80
Dwayne Applewhite <dwayneaapplewhite@gmail.com>
2022-07-18 14:44:19 -0400

    added event listener for enter button

--- js/index.js
+++ js/index.js
@@ -7,15 +7,22 @@ const api = `https://api.datamuse.com/words?rel_rhy=`; //api url
 
 searchBtn.addEventListener("click", getWord);
 
-/** 
+searchInput.addEventListener("keyup", function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        searchBtn.click(); 
+    }
+});
+
+/**
  * gets user input value and fetches the api and returns word in paragraph
  */ 
 async function getWord(){
-    pText.textContent= ""; //clear paragraph
+   section.textContent= ""; //clear paragraph
     const response = await fetch(api + searchInput.value);
     const data = response.json();
-    for (let i =0; i < 9;i++)
-        data.then((data) => pText.textContent += data[i].word + " ")
+    for (let i =0; i < 99 ;i++)
+        data.then((data) => section.innerHTML += `<p id="word${i}">${data[i].word}</p>` + " ")
 }
 
 /**
